6a896988f6d615dd9179db911572bb95d7603e43
WooJin Park <ak2j38@gmail.com>
2022-03-15 14:16:36 +0900

    feature : 1_1 코드 구현
    1. 책의 예제인 User 대신 현재 미션과 비슷한 Article로 수정
    2. mysql 대신 간단한 h2 DB 사용
    
    end

---
+++ chapter_01/ader/src/Application.java
@@ -0,0 +1,29 @@
+import dao.ArticleDao;
+import domain.Article;
+
+import java.sql.SQLException;
+import java.time.LocalDateTime;
+
+public class Application {
+
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        ArticleDao articleDao = new ArticleDao();
+
+        Article article = new Article();
+        article.setId(1L);
+        article.setWriter("ader");
+        article.setTitle("title test");
+        article.setContents("contents test");
+        article.setCreatedTime(LocalDateTime.now());
+        article.setUpdatedTime(LocalDateTime.now());
+
+        articleDao.add(article);
+        System.out.println("등록 성공");
+
+        Article findArticle = articleDao.get(article.getId());
+        System.out.println("findArticle.getWriter() = " + findArticle.getWriter());
+        System.out.println("findArticle.getTitle() = " + findArticle.getTitle());
+
+        System.out.println(findArticle.getId() + " 조회 성공");
+    }
+}
